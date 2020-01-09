ca3c35edcf5c5904431053545d971c225f8cc1d0
leilei <248721866@qq.com>
2020-01-09 16:56:33 +0800

    Initial commit

---
+++ src/main/java/com/leilei/fastdfs/SpringBootFastDfsApplication.java
@@ -0,0 +1,13 @@
+package com.leilei.fastdfs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFastDfsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootFastDfsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/leilei/fastdfs/SpringBootFastDfsApplicationTests.java
@@ -0,0 +1,17 @@
+package com.leilei.fastdfs;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootFastDfsApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
